1d5111c0e6cb9e3c1b4f284214def9ca8fadf08a
Andrey Cespedes <andrey42002@gmail.com>
2025-03-12 09:51:47 -0500

    añadiendo practica gestor de alumnos

---
+++ gestor_De_Alumnos/src/main/java/gestorDeAlumnos/GestorDeAlumnosApplication.java
@@ -0,0 +1,13 @@
+package gestorDeAlumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeAlumnosApplication.class, args);
+	}
+
+}
