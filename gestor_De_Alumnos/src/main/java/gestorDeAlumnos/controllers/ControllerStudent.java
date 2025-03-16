d03547ee2e90a573ecd98b9ff3516a12c39007a6
Andrey Cespedes <andrey42002@gmail.com>
2025-03-16 12:08:51 -0500

    Agregando el metodo para encontrar al estudiante por medio del correo

--- gestor_De_Alumnos/src/main/java/gestorDeAlumnos/controllers/ControllerStudent.java
+++ gestor_De_Alumnos/src/main/java/gestorDeAlumnos/controllers/ControllerStudent.java
@@ -2,6 +2,7 @@ package gestorDeAlumnos.controllers;
 
 import gestorDeAlumnos.domain.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,6 +22,16 @@ public class ControllerStudent {
     @GetMapping("/Alumnos")
     public List<Student> getStudents(){
         return students;
+    }
+
+    @GetMapping("/Alumnos/{email}")
+    public Student getAlumno (@PathVariable String email) {
+        for (Student S : students) {
+            if(S.getEmail().equalsIgnoreCase(email)){
+                return S;
+            }
+        }
+        return null;
     }
 
 }
